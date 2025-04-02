81fe459cf4a3f5a6557e4989f532550d9764ccbc
metalhead0 <147883336+metalhead0@users.noreply.github.com>
2025-04-02 04:03:11 +0200

    add 2 dice and button add all

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+  let diceAray = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")]
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageView1.image = diceAray.randomElement()
+        diceImageView2.image = diceAray.randomElement()
+        
+        
     }
 
+    @IBOutlet weak var diceImageView2: UIImageView!
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    
 
 }
 
